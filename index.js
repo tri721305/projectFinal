ebe367fca81f1a6e788228ec1ee513e2eb324b34
DHMTri <dhmt721305@gmail.com>
2020-08-06 15:46:39 +0700

    tri update

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var lowdb = require('lowdb')
+var fileSync = require('lowdb/adapters/FileSync')
+var shortid = require('shortid')
+//
+var adapter = new fileSync('db.json')
+var db = lowdb(adapter)
+db.defaults({listUsers: []}).write()
 // port
 var port = 4080
-
-var listUsers = [
-  {id: 1, name: 'Tri'},
-  {id: 2, name: 'Truck'},
-  {id: 3, name: 'Kimteahea'}
-]
+//
+//
+// var listUsers = [
+//   {id: 1, name: 'Tri'},
+//   {id: 2, name: 'Truck'},
+//   {id: 3, name: 'Kimteahea'}
+// ]
 // set view engine using pug, 
 
 app.set('view engine', 'pug')
@@ -25,14 +33,16 @@ app.get('/', function(req, res){
 
 app.get('/users', function(req, res){
   res.render('users/index.pug',{
-    listUsers: listUsers
+    listUsers: db.get('listUsers').value()
   })
 })
 
+
 app.get('/users/search', function(req,res){
   var q = req.query.q;
-  var temp = listUsers.filter(function(listUsers){
-    return listUsers.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+  // var listUsers = db.get('listUsers')
+  var temp = db.get('listUsers').value().filter(function(x){
+    return x.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
   })
   res.render('users/index',{
     listUsers : temp
@@ -44,11 +54,22 @@ app.get('/users/create', function(req, res){
 })
 
 app.post('/users/create', function(req, res){
+  req.body.id = shortid.generate();
   console.log(req.body)
-  listUsers.push(req.body)
+  db.get('listUsers').push(req.body).write()
   res.redirect('/users')
 })
 
+app.get('/users/:id', function(req, res){
+  var id = req.params.id;
+  console.log(id)
+  console.log(typeof(id))
+  var listUsers = db.get('listUsers').find({id:id}).value()
+  console.log(listUsers)
+  res.render('users/view',{
+    listUsers:listUsers
+  })
+})
 app.listen(port, ()=> {
   console.log('Example app is listening at port' + port )
 })
